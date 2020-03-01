3e16cab60ca4f78592bccf006d0f48f36764986b
Karolina-rm <karolina.ra.kr@gmail.com>
2020-03-01 11:19:54 +0100

    Task 14.1 first version

---
+++ Spring/src/main/java/Spring/Forum/ForumUser.java
@@ -0,0 +1,17 @@
+package Spring.Forum;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ForumUser {
+
+    private String username;
+
+    public ForumUser() {
+        username = "John Smith";
+    }
+
+    public String getUsername() {
+        return username;
+    }
+}
